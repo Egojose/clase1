1cc263daeaf18044200d1e11ad54fa7ed0f99e30
jose <jose.perez@gmfinancial.com>
2023-11-22 15:35:07 -0500

    corrige error en el valor del campo select

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 function processInput() {
     // Get values from input fields
-    var foodType = document.getElementById('foodType').value;
+    var foodType = document.getElementById('foodType');
+    var selectedFoodIndex = foodType.selectedIndex;
+
+    // Get the selected option value
+    var selectedFoodType = foodType.options[selectedFoodIndex].value;
     var toppings = document.getElementById('toppings').value;
 
     // Validate if both fields are filled
-    if (foodType && toppings) {
+    if (selectedFoodType && toppings) {
         // Display the options in the 'root' div
         var rootDiv = document.getElementById('root');
-        rootDiv.innerHTML = "You selected: " + foodType + " with toppings: " + toppings;
+        rootDiv.innerHTML = "Seleccionaste: " + selectedFoodType + " con toppings: " + toppings;
     } else {
         alert('Please fill in both fields.');
     }
